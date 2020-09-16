385d6a6a3858d9acc0ae0138fd289da8485c3e47
Shen Feiyu <heroicbig@126.com>
2020-09-16 21:19:20 +0800

    Initial Commit

---
+++ Mumsh/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Mumsh
+//
+//  Created by Francis on 2020/9/16.
+//  Copyright © 2020 Francis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
